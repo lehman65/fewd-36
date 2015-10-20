df9556444045eb5011ceb6b0d7c90f85116f9241
terrylehman <tlehman65@att.net>
2015-10-19 21:39:02 -0700

    2nd commit color switcher

--- Color-Switcher/color-switcher.js
+++ Color-Switcher/color-switcher.js
@@ -3,3 +3,4 @@ var color2 = document.querySelector(".salmon");
 var color3 = document.querySelector(".burlywood");
 var color4 = document.querySelector(".thistle");
 var body = document.querySelector("body");
+body.setAttribute("class", "olive");
