47cffac2190dfa114fb4fc531e16915d489af33a
terrylehman <tlehman65@att.net>
2015-10-26 02:09:33 -0700

    adding input class and some js

---
+++ temperature-converter/js/temp-con.js
@@ -0,0 +1,31 @@
+var tempF = document.querySelector('.temp-f');
+var tempC = document.querySelector('.temp-c');
+
+tempF.addEventListener('keyup', calcFtoC);
+tempC.addEventListener('keyup', calcCtoF);
+
+function calcFtoC(event) {
+  if (tempF.value.length > 0) {
+    tempC.value = calcFahrenheitToCelcius(tempF.value)
+  } else {
+    tempC.value = '';
+  }
+}
+
+function calcFahrenheitToCelcius(tempF) {
+  var tempC = (tempF-32) * (5/9);
+    return Math.round(tempC);
+}
+
+function calcCtoF(event) {
+  if (tempC.value.length > 0) {
+    tempF.value = calcCelciusToFahrenheit(tempC.value)
+  } else {
+    tempF.value = '';
+  }
+}
+
+function calcCelciusToFahrenheit(tempC) {
+  var tempF = tempC * 9/5 + 32;
+    return  Math.round(tempF);
+}
