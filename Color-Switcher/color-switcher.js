c43b9e536ca7464d7c39d1bfa789155200946ffe
terrylehman <tlehman65@att.net>
2015-10-27 02:02:52 -0700

    fixes

--- Color-Switcher/color-switcher.js
+++ Color-Switcher/color-switcher.js
@@ -1,11 +1,20 @@
-(var color1 = document.querySelector(".olive");
+var color1 = document.querySelector(".olive");
 var color2 = document.querySelector(".salmon");
 var color3 = document.querySelector(".burlywood");
 var color4 = document.querySelector(".thistle");
 var body = document.querySelector("body");
 body.setAttribute("class", "olive");
 
+
+
 function changeColor(color) {
-	var body = document.querySelector("body")
-	body.setAttribute("class", color);
+	var body = document.querySelector(".olive")
+	console.log(test.className);
+	test.classList.remove("olive");
+	test.classList.add("salmon");
+	test.classList.add("burlywood");
+	test.classList.add("thistle");
+	console.log(test.className);
+
+	
 }
