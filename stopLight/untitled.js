07d6bbfa8face566f1a52b28bdf8533d74e1ebca
terrylehman <tlehman65@att.net>
2015-11-03 18:20:41 -0800

    more classwork

--- stopLight/untitled.js
+++ stopLight/untitled.js
@@ -1,13 +1,20 @@
 var red = document.querySelector(".stop");
 var yellow = document.querySelector(".slow");
 var green = document.querySelector(".go");
-
-
+var button1 = document.querySelector(".button1");
+var button2 = document.querySelector(".button2");
+var button3 = document.querySelector(".button3");
 // Events
 
-red.addEventListener("click", onRed);
-yellow.addEventListener("click", onYellow);
-green.addEventListener("click", onGreen);
+//red.addEventListener("click", onRed);
+//yellow.addEventListener("click", onYellow);
+//green.addEventListener("click", onGreen);
+button1.addEventListener("click", onGreen);
+button1.addEventListener("click", onYellow);
+button2.addEventListener("click", onRed);
+button2.addEventListener("click", onGreen);
+button3.addEventListener("click", onRed);
+button3.addEventListener("click", onYellow);
 // Functions
 
 function onRed() {
