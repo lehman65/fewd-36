f572fa1ceafe39bbcab76962c4588101610863be
terrylehman <tlehman65@att.net>
2015-11-11 21:01:42 -0800

    event delegation

--- color-switcher 2/js/color-switcher.js
+++ color-switcher 2/js/color-switcher.js
@@ -5,7 +5,7 @@ var color2 = document.querySelector(".skyblue");
 var color3 = document.querySelector(".salmon");
 var color4 = document.querySelector(".burlywood");
 var color5 = document.querySelector(".thistle");
-
+var lists = document.querySelector("ul");
 
 // Events
 // -------------------------------------
@@ -14,15 +14,20 @@ color2.addEventListener("click", changeColor)
 color3.addEventListener("click", changeColor)
 color4.addEventListener("click", changeColor)
 color5.addEventListener("click", changeColor)
+lists.addEventListener("click", listsChange)
 // ...to do in next class!
 
 
 // Event handler functions
 // -------------------------------------
+function listsChange(event) {
+console.log("listsChange");
+}
+
 function changeColor(event) {
 	var swatch = event.target;
 	var color = swatch.className;
-	
+	var color = swatch.getAttribute("class");
 
 	var body = document.querySelector("body");
 	body.className = color;
